249dbadcd478e80640e27359a8a70c04ac3abddf
atharva939 <57951022+atharva939@users.noreply.github.com>
2026-03-17 15:00:14 +0530

    Create Day-1.py

---
+++ week-1/Day-1.py
@@ -0,0 +1,24 @@
+Name = "Atharvaa" #Variable
+price = 20 #also a variable
+
+print(price)
+
+#diffrent type of data types
+name = "Atharvaa" #string
+age = 20 #integer
+height = 5.8 #float
+is_student = True #boolean
+
+#input output operatiosn
+name = input("Enter your name: ") #taking input from user
+print("Hello, " + name + "!") #outputting a greeting message
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#type conversion
+age = int(input("Enter your age: ")) #converting input to integer
+height = float(input("Enter your height: ")) #converting input to float
+print("Your age is " + str(age) + " and your height is " + str(height) + ".") #outputting
