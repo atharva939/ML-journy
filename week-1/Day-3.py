65531f47e5459ce1b74c1c1b20fc0446d078f5b8
Atharva C Lakade <57951022+atharva939@users.noreply.github.com>
2026-03-22 11:25:51 +0530

    Add loop examples in Day-3.py
    Added examples of for loops, while loops, break, and continue statements.

---
+++ week-1/Day-3.py
@@ -0,0 +1,25 @@
+#for loops
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+
+#while loop
+x = 0
+while x < 4:
+    print(x)
+    x += 1  
+  
+#break
+x = 0
+for index in range(10):
+    x = index * 10
+    if index == 5:
+    	break
+    print(x)
+
+#Continue
+  for index in range(3, 8): 
+    x = index * 10
+    if index == 5:
+    	continue
+    print(x)
